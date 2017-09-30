96a5d0297efb3a59b719db0da8afe7fe17f852a2
idzik78 <Tomasz Idzikowski>
2017-09-30 15:09:14 +0200

    Zadanie 6

---
+++ src/pl/tomaszidzikowski/klasydrugie/Zadanie6.java
@@ -0,0 +1,11 @@
+package pl.tomaszidzikowski.klasydrugie;
+/*
+Napisz program, który wypisuje liczby naturalne od 1 do 100.
+ */
+public class Zadanie6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=100 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
