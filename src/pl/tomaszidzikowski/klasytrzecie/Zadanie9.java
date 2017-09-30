b1f4476cfcd78c06bc23a3ad14603a9a2835d8e1
idzik78 <Tomasz Idzikowski>
2017-09-30 14:31:02 +0200

    Zadanie 9

---
+++ src/pl/tomaszidzikowski/klasytrzecie/Zadanie9.java
@@ -0,0 +1,21 @@
+package pl.tomaszidzikowski.klasytrzecie;
+
+import java.util.Scanner;
+
+/*Napisz funkcję, która stwierdza czy liczba jest pierwsza
+ */
+public class Zadanie9 {
+    public static boolean isPrime(int number){
+        for (int i = 2; i <= Math.ceil(Math.sqrt(number)); i++) {
+            if (number%i==0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your number");
+        int number=scanner.nextInt();
+        if (isPrime(number)) System.out.println("Your number is prime");
+        else System.out.println("Your number is not prime");
+    }
+}
