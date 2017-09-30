2a50fdac29da929f9d8e3279fe47dc523e920218
idzik78 <Tomasz Idzikowski>
2017-09-30 14:55:13 +0200

    Zadanie 1

---
+++ src/pl/tomaszidzikowski/klasydrugie/Zadanie1.java
@@ -0,0 +1,11 @@
+package pl.tomaszidzikowski.klasydrugie;
+/*
+Napisz program, który deklaruje 2 zmienne całkowite i na ekran wypisuje ich sumę.
+ */
+public class Zadanie1 {
+    public static void main(String[] args) {
+        int firstNumber=5;
+        int secondNumber=13;
+        System.out.println("The sum is "+(firstNumber+secondNumber));
+    }
+}
