ca8ea203a8a0a3d4ee9e39c01b1509492ed2e3d1
idzik78 <Tomasz Idzikowski>
2017-09-30 15:45:32 +0200

    Zadanie 16

---
+++ src/pl/tomaszidzikowski/klasydrugie/Zadanie16.java
@@ -0,0 +1,19 @@
+package pl.tomaszidzikowski.klasydrugie;
+
+import java.util.Scanner;
+
+public class Zadanie16 {
+    public static boolean isPrime(int number){
+        for (int i = 2; i <= Math.ceil(Math.sqrt(number)); i++) {
+            if (number%i==0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your number");
+        int number=scanner.nextInt();
+        if (isPrime(number)) System.out.println("Your number is prime");
+        else System.out.println("Your number is not prime");
+    }
+}
