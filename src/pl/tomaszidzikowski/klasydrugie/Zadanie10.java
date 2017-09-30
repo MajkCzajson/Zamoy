e06a732843bfd5c847bbf1b442e4a4a27c92f2b2
idzik78 <Tomasz Idzikowski>
2017-09-30 15:17:40 +0200

    Zadanie 10

---
+++ src/pl/tomaszidzikowski/klasydrugie/Zadanie10.java
@@ -0,0 +1,19 @@
+package pl.tomaszidzikowski.klasydrugie;
+
+import java.util.Scanner;
+
+/*
+Napisz program, który odczytuje imię i sprawdza czy jest ono palindromem.
+ */
+public class Zadanie10 {
+    public static boolean isPalindrome(String word){
+        return word.equals(new StringBuilder(word).reverse().toString());
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your word");
+        String word=scanner.nextLine();
+        if (isPalindrome(word)) System.out.println("This is palindrome");
+        else System.out.println("This is not palindrome");
+    }
+}
